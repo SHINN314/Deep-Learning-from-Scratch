7a0b477ad05b608bd82b433038adfc1009ec97c8
SHINN314 <shinnosukekimura8@gmail.com>
2025-02-06 21:01:43 +0900

    ミニバッチの実装

--- one/chapter4/mini_batch.py
+++ one/chapter4/mini_batch.py
@@ -5,7 +5,10 @@ from common.get_mnist_data import get_mnist_data
 
 if __name__=="__main__":
   x_train, t_train, x_test, t_test = get_mnist_data()
-  print(x_train.shape)
-  print(t_train.shape)
-  print(x_test.shape)
-  print(t_test.shape)
+  train_size = x_train.shape[0]
+  batch_size = 10
+  batch_mask = np.random.choice(train_size, batch_size)
+  x_batch = x_train[batch_mask]
+  t_batch = t_train[batch_mask]
+  print(x_batch) # test code
+  print(t_batch) # test code
