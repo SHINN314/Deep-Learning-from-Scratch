d3819a21deda00b4e0a880445ff907320c2decbb
SHINN314 <shinnosukekimura8@gmail.com>
2025-02-06 21:14:08 +0900

    クロスエントロピー誤差の修正

--- one/chapter4/cross_entropy_error.py
+++ one/chapter4/cross_entropy_error.py
@@ -2,7 +2,13 @@ import numpy as np
 
 def cross_entropy_error(y, t):
   delta = 1e-7 # log(0)防止のため
-  return -np.sum(t * np.log(y + delta))
+  if y.ndim == 1:
+    # batch_size == 1のときに配列の形を変える
+    t = t.reshape(1, t.size)
+    y = y.reshape(1, y.size)
+
+  batch_size = y.shape[0]
+  return -np.sum(t * np.log(y + delta)) / batch_size
 
 if __name__=="__main__":
   t = np.array([0, 0, 1, 0, 0, 0, 0, 0, 0, 0])
