fc1109ff40298d43f84e30ad1c6d1015f2a7da1f
SHINN314 <shinnosukekimura8@gmail.com>
2025-02-23 01:24:15 +0900

    勾配を求めた

--- one/chapter4/pertial_diff.py
+++ one/chapter4/pertial_diff.py
@@ -2,21 +2,29 @@ import numpy as np
 import matplotlib.pyplot as plt
 from diff import numerical_diff
 
-def function_2(x, y):
-  return x**2 + y**2
+def function_2(x):
+  return x[0]**2 + x[1]**2
+
+# ベクトルxにおける勾配を求める関数
+def numerical_gradient(f, x):
+  h = 1e-4
+  grad = np.zeros_like(x)
+
+  for idx in range(x.size):
+    tmp_val = x[idx]
+
+    # f(x + h)の計算
+    x[idx] = tmp_val + h
+    fxh1 = f(x)
+
+    # f(x - h)の計算
+    x[idx] = tmp_val - h
+    fxh2 = f(x)
+
+    grad[idx] = (fxh1 - fxh2) / (2.0*h)
+    x[idx] = tmp_val # 値の初期化
+
+  return grad
 
 if __name__ == "__main__":
-  # set data
-  x = np.arange(-3, 3, 0.1)
-  y = np.arange(-3, 3, 0.1)
-
-  # 格子点の情報をnumpy arrayに保存
-  X, Y = np.meshgrid(x, y)
-  Z = function_2(X, Y)
-
-  fig = plt.figure()
-  ax = fig.add_subplot(projection="3d")
-  ax.plot_surface(X, Y, Z)
-  ax.set_aspect("auto")
-  plt.savefig("figure2.png")
- 
+  print(numerical_gradient(function_2, np.array([0.0, 2.0])))
