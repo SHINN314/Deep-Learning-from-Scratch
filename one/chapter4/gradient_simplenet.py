f87275f4cc93ec40275da56d423bb9fe3e1afcd2
SHINN314 <shinnosukekimura8@gmail.com>
2025-02-28 14:16:46 +0900

    でけた

--- one/chapter4/gradient_simplenet.py
+++ one/chapter4/gradient_simplenet.py
@@ -1,7 +1,17 @@
 import sys, os
 sys.path.append(os.pardir)
+import numpy as np
 import common.networks as networks
+import common.gradient as gradient
 
 if __name__=="__main__":
   network = networks.simpleNet()
-  print(network.W)
+  x = np.array([0.6, 0.9]) # 入力
+  p = network.predict(x)
+  t = np.array([0, 0, 1]) # 正解ラベル
+
+  def f(W):
+    return network.loss(x, t)
+    
+  dW = gradient.numerical_gradient(f, network.W)
+  print(dW)
