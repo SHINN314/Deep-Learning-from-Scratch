011c50dd49e172ed9b1623ff650751975ca06f8c
SHINN314 <shinnosukekimura8@gmail.com>
2025-05-13 02:25:00 +0900

    乗算レイヤーの実装

---
+++ one/chapter5/layer_naive.py
@@ -0,0 +1,55 @@
+class MulLayer:
+  """
+  Multiplication layer
+
+  Attributes
+  ----------
+  x : numpy.ndarray
+      Input data
+  y : numpy.ndarray
+      Input data
+  """
+  def __init__(self):
+    self.x = None
+    self.y = None
+
+  def forward(self, x, y):
+    """
+    Forward pass
+    
+    Parameters
+    ----------
+    x : numpy.ndarray
+        Input data
+    y : numpy.ndarray
+        Input data
+    
+    Returns
+    -------
+    numpy.ndarray
+        Output data
+    """
+    self.x = x
+    self.y = y
+    out = x * y
+
+    return out
+  
+  def backward(self, dout):
+    """
+    Backward pass
+    
+    Parameters
+    ----------
+    dout : numpy.ndarray
+      Gradient of the loss with respect to the output
+    
+    Returns
+    -------
+    tuple
+      Gradients of the loss with respect to the inputs
+    """
+    dx = dout * self.y
+    dy = dout * self.x
+
+    return dx, dy
